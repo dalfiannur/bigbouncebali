81ccb2ef7b2febbf38112c131c444c39434261ee
dalfiannur <dalfiannur@gmail.com>
2025-07-31 17:14:00 +0700

    Remove unused badge element from attraction section layout

--- src/components/attraction-section.tsx
+++ src/components/attraction-section.tsx
@@ -1,5 +1,4 @@
 import {Attraction} from '@/types/attraction'
-import {Badge} from '@/components/ui/badge'
 import {Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger} from '@/components/ui/dialog'
 import {Card, CardDescription, CardTitle} from './ui/card'
 import Image from 'next/image'
