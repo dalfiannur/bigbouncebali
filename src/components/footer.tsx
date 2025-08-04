be748159590cde732c8e864de47a43aee29948f4
dalfiannur <dalfiannur@gmail.com>
2025-08-04 13:25:06 +0700

    Redesign Footer: replace social links and static navigation with accordion layout; update structure, styling, and content for better organization and accessibility.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,45 +1,65 @@
 import Link from 'next/link'
-import {SiInstagram, SiTiktok} from '@icons-pack/react-simple-icons'
 import Image from 'next/image'
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from '@/components/ui/accordion'
 
 export const Footer = () => {
 	return (
 		<footer
-			className="flex flex-col mb-14 md:mb-0 p-6">
-			<div className="flex justify-center items-center gap-6 ">
-				<Link href="/">
-					<SiInstagram className="text-white hover:text-white" size="2em" title="Instagram" />
-				</Link>
-				<Link href="/">
-					<SiTiktok className="text-white hover:text-white" size="2em" title="Tiktok" />
-				</Link>
-			</div>
-			<div className="mt-6 flex justify-center items-center gap-6 ">
-				<Link href="/" className="text-white uppercase font-bold underline">
-					Magenta
-				</Link>
-				<Link href="/" className="text-white uppercase font-bold underline">
-					Incubase Studio
-				</Link>
-			</div>
+			className="flex flex-col mb-14 md:mb-0 p-6 bg-orange-500 text-white">
+			<Accordion type="single" collapsible>
+				<AccordionItem value="stay-tuned">
+					<AccordionTrigger className="font-extrabold text-xl">Stay Tuned</AccordionTrigger>
+					<AccordionContent className="flex flex-col gap-4 text-balance font-semibold">
+						<Link href="/" className="underline underline-offset-4 ">
+							Instagram
+						</Link>
+						<Link href="/" className="underline underline-offset-4 ">
+							Tiktok
+						</Link>
+					</AccordionContent>
+				</AccordionItem>
+				
+				<AccordionItem value="information">
+					<AccordionTrigger className="font-extrabold text-xl">Information</AccordionTrigger>
+					<AccordionContent className="flex flex-col gap-4 text-balance font-semibold">
+						<Link href="/" className="underline underline-offset-4 ">
+							FAQs
+						</Link>
+						<Link href="/" className="underline underline-offset-4 ">
+							Terms of Service
+						</Link>
+						<Link href="/" className="underline underline-offset-4 ">
+							Privacy Policy
+						</Link>
+						<Link href="/" className="underline underline-offset-4 ">
+							Weather Policy
+						</Link>
+					</AccordionContent>
+				</AccordionItem>
+				
+				<AccordionItem value="get-in-touce">
+					<AccordionTrigger className="text-white font-extrabold text-xl">Get In Touch</AccordionTrigger>
+					<AccordionContent className="flex flex-col gap-4 text-balance white font-semibold">
+						<Link href="/faqs" className="underline underline-offset-4 ">
+							Contact Us
+						</Link>
+						<Link href="/terms" className="underline underline-offset-4 ">
+							Magenta
+						</Link>
+						<Link href="/privacy" className="underline underline-offset-4 ">
+							Incubase Studio
+						</Link>
+						<Link href="/weather" className="underline underline-offset-4 ">
+							The Big Bounce America
+						</Link>
+					</AccordionContent>
+				</AccordionItem>
+			</Accordion>
 			<div className="mt-6 flex justify-center items-center">
-				<Image src="/img/magenta.webp" alt="Magenta & Incubase Studio" width={150} height={90} />
+				<Image src="/img/magenta.webp" alt="Magenta & Incubase Studio" width={150} height={90}/>
 			</div>
-			<nav className="mt-6 flex justify-center gap-4">
-				<Link href="/faqs" className="text-xs underline underline-offset-4 text-gray-200">
-					FAQs
-				</Link>
-				<Link href="/terms" className="text-xs underline underline-offset-4 text-gray-200">
-					Terms of Service
-				</Link>
-				<Link href="/privacy" className="text-xs underline underline-offset-4 text-gray-200">
-					Privacy Policy
-				</Link>
-				<Link href="/weather" className="text-xs underline underline-offset-4 text-gray-200">
-					Weather Policy
-				</Link>
-			</nav>
-			<p className="mt-6 text-xs text-center text-white">© {new Date().getFullYear()} The Big Bounce Bali. All rights reserved.</p>
+			<p className="mt-6 text-xs text-center text-white">© {new Date().getFullYear()} The Big Bounce Bali. All
+				rights reserved.</p>
 		</footer>
 	)
 }
