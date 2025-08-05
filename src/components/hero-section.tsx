4ce7d368c313b51796a7afbc765d78f7fe0fe756
dalfiannur <dalfiannur@gmail.com>
2025-08-05 16:11:10 +0700

    Simplify Hero Section: remove bottom image for cleaner layout; adjust styling and structure for improved consistency.

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -8,9 +8,8 @@ export const HeroSection = () => {
 	
 	return (
 		<section
-			className="w-full h-screen relative overflow-hidden">
-			<Image src="/img/hero.webp" alt="The Big Bounce Bali" width={1920} height={1318} className="w-full" />
-			<Image src="/img/hero-bottom.webp" alt="The Big Bounce Bali" width={1920} height={331} className="hidden md:block w-full" />
+			className="w-full relative overflow-hidden">
+			<Image src="/img/hero.webp" alt="The Big Bounce Bali" width={1920} height={1318} className="w-full"/>
 			<button
 				className={cn('absolute hidden md:block -mt-4 md:mt-72 bg-gradient-to-r from-[#f5c764] to-[#fa8c6f] hover:to-[#f5c764] hover:from-[#fa8c6f] text-white px-4 md:px-12 py-2 md:py-8 text-sm md:text-5xl font-bold rounded-3xl cursor-pointer animate-bounce', fonts.beachday.className)}
 			>
